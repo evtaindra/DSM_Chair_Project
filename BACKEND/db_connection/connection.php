0cd4a80fa8140ff2ac5daacaa633aea5de1847db
Josue <josue.ardon113@gmail.com>
2019-05-14 00:28:48 -0600

    mysqli connection/ utf8 support for both connections/ pdo categories endpoint

--- BACKEND/db_connection/connection.php
+++ BACKEND/db_connection/connection.php
@@ -1,16 +1,17 @@
 <?php
-    $server_name = "localhost";
-    $db_name = "db_stories";
-    $user = "root";
-    $password = "";
-    $charset = "utf8";
 
-    try {
-        $connection = new PDO("mysql:host=$server_name; dbname=$db_name;", $user, $password);
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
+$link = 'mysql:host=localhost;dbname=db_stories2;charset=utf8';
+$server = 'localhost';
+$db = 'db_stories2';
+$user = 'root';
+$password = '';
 
-    } catch(PDOException $e) {
-        echo "Conection failed: " . $e->getMessage();
-    }
-?>
+try{
+    $pdo = new PDO($link,$user,$password);
+    $connection = new mysqli($server, $user, $password, $db);
+    mysqli_set_charset($connection,"utf8");
+    //echo "SUCCESSFUL CONNECTION DB_STORIES";
+}catch(PDOException $e){
+    print '¡ERROR!: ' . $e -> getMessage()."</br>";
+    die();
+}
--- BACKEND/endpoints/read_categories.php
+++ BACKEND/endpoints/read_categories.php
@@ -1,45 +1,15 @@
 <?php
 
-    require_once "../db_connection/connection.php";
+require_once "../db_connection/connection.php";
 
-    $categories = array();
-    //Fabulas, Aventuras, Leyendas, Terror
+//Lista categorias
+$query = 'SELECT * FROM categories';
 
-    Class Category {
+$sql = $pdo -> prepare($query);
+$sql -> execute();
+$list_categories = $sql -> fetchAll();
 
-        public function __construct($category_id, $name_category, $image_name, $image_format, $image_route) {
+header('Content-Type: application/json');
+echo json_encode($list_categories);
 
-            $this->category_id = $category_id;
-            $this->name_category = $name_category;
-            $this->image_name = $image_name;
-            $this->image_format = $image_format;
-            $this->image_route = $image_route;
-
-        }
-
-    }
-
-    $query = "SELECT * FROM categories";
-
-    $result = $connection->query($query);
-
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $category = new Category($row['category_id'],
-                $row['name_category'],
-                $row['image_name'],
-                $row['image_format'],
-                $row['image_route']);
-
-            array_push($categories, $category);
-
-
-        }
-        
-    } else {
-        echo "0 results";
-    }
-
-    header('Content-Type: application/json');
-    echo json_encode($categories);
 ?>
